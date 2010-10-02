99eec8d564db8f5822bca3a55d6d5fa64fdfb408
Michał Matyas <michal@6irc.net>
2010-10-02 18:30:04 +0200

    Moved Haml cache directory to APPPATH.'cache/' instead of APPPATH.'views/'

--- config/phamlp.php
+++ config/phamlp.php
@@ -6,7 +6,7 @@ return array
 		'lib_dir' => realpath(dirname(__FILE__).'/../vendor/phamlp_2.2_r0094/').'/',
 		'haml'    => array
 		(
-			'cache_dir' => '_compiled/', // cache_dir is a directory within APPPATH.'views/'
+			'cache_dir' => 'haml/', // cache_dir is a directory within APPPATH.'cache/'
 			'extension' => '.haml', // HAML template file extension
 			'options'   => array
 			(
@@ -16,7 +16,7 @@ return array
 				'emptyTags'           => array('meta', 'img', 'link', 'br', 'hr', 'input', 'area', 'param', 'col', 'base'),
 				'escapeHtml'          => false,
 				'filterDir'           => null,
-				'format'              => 'xhtml',
+				'format'              => 'html5',
 				'inlineTags'          => array('a', 'abbr', 'accronym', 'b', 'big', 'cite', 'code', 'dfn', 'em', 'i', 'kbd', 'q', 'samp', 'small', 'span', 'strike', 'strong', 'tt', 'u', 'var'),
 				'minimizedAttributes' => array('compact', 'checked', 'declare', 'readonly', 'disabled', 'selected', 'defer', 'ismap', 'nohref', 'noshade', 'nowrap', 'multiple', 'noresize'),
 				'preserve'            => array('pre', 'textarea'),
