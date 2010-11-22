7c1e3b6f6744a61ab58facc53d3bcad434fd2375
Michał Matyas <michal@6irc.net>
2010-11-22 16:35:27 +0100

    fixed config

--- config/phamlp.php
+++ config/phamlp.php
@@ -3,7 +3,7 @@ return array
 (
 	'phamlp' => array
 	(
-		'lib_dir' => realpath(dirname(__FILE__).'/../vendor/phamlp_2.2_r0094/').'/',
+		'lib_dir' => realpath(dirname(__FILE__).'/../vendor/d4rky-pl-phamlp/').'/',
 		'haml'    => array
 		(
 			'cache_dir' => 'haml/', // cache_dir is a directory within APPPATH.'cache/'
