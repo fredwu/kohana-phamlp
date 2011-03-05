099be5186221ea0594ab8eecb3914b07a03c4fd7
Michał Matyas <michal@6irc.net>
2011-03-06 00:59:38 +0100

    Rebuilt module to load PHamlP 'kohana way' (find_file, init.php) WARNING: config.php breaks backward compatibility!

--- config/phamlp.php
+++ config/phamlp.php
@@ -1,31 +1,30 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 return array
 (
-	'phamlp' => array
+	'parsers' => array('haml'),
+	'lib_dir' => 'd4rky-pl-phamlp/', // depends on phamlp version
+	'haml'    => array
 	(
-		'lib_dir' => realpath(dirname(__FILE__).'/../vendor/d4rky-pl-phamlp/').'/',
-		'haml'    => array
+		'cache_dir' => 'haml/', // cache_dir is a directory within APPPATH.'cache/'
+		'extension' => '.haml', // HAML template file extension
+		'options'   => array
 		(
-			'cache_dir' => 'haml/', // cache_dir is a directory within APPPATH.'cache/'
-			'extension' => '.haml', // HAML template file extension
-			'options'   => array
-			(
-				'attrWrapper'         => '"',
-				'debug'               => 0,
-				'doctype'             => null,
-				'emptyTags'           => array('meta', 'img', 'link', 'br', 'hr', 'input', 'area', 'param', 'col', 'base'),
-				'escapeHtml'          => false,
-				'filterDir'           => null,
-				'format'              => 'html5',
-				'inlineTags'          => array('a', 'abbr', 'accronym', 'b', 'big', 'cite', 'code', 'dfn', 'em', 'i', 'kbd', 'q', 'samp', 'small', 'span', 'strike', 'strong', 'tt', 'u', 'var'),
-				'minimizedAttributes' => array('compact', 'checked', 'declare', 'readonly', 'disabled', 'selected', 'defer', 'ismap', 'nohref', 'noshade', 'nowrap', 'multiple', 'noresize'),
-				'preserve'            => array('pre', 'textarea'),
-				'preserveComments'    => false,
-				'style'               => 'nested',
-				'suppressEval'        => false,
-				'ugly'                => false,
-				'helperFile'          => null,
-			)
-		),
+			'attrWrapper'         => '"',
+			'debug'               => 0,
+			'doctype'             => null,
+			'emptyTags'           => array('meta', 'img', 'link', 'br', 'hr', 'input', 'area', 'param', 'col', 'base'),
+			'escapeHtml'          => false,
+			'filterDir'           => null,
+			'format'              => 'html5',
+			'inlineTags'          => array('a', 'abbr', 'accronym', 'b', 'big', 'cite', 'code', 'dfn', 'em', 'i', 'kbd', 'q', 'samp', 'small', 'span', 'strike', 'strong', 'tt', 'u', 'var'),
+			'minimizedAttributes' => array('compact', 'checked', 'declare', 'readonly', 'disabled', 'selected', 'defer', 'ismap', 'nohref', 'noshade', 'nowrap', 'multiple', 'noresize'),
+			'preserve'            => array('pre', 'textarea'),
+			'preserveComments'    => false,
+			'style'               => 'nested',
+			'suppressEval'        => false,
+			'ugly'                => false,
+			'helperFile'          => null,
+		)
 	),
+
 );
---
+++ init.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = Kohana::config('phamlp');
+if ( in_array('haml', $config['parsers']) && ! class_exists('HamlParser'))
+{
+	$haml_dir = $config['lib_dir']. 'haml';
+	
+	if (!$haml_dir)
+	{
+		throw new Exception("Cannot find phamlp's HAML directory.");
+	}
+
+	require_once(Kohana::find_file('vendor', $haml_dir.'/HamlParser'));
+}
+
+if ( in_array('sass', $config['parsers']) && ! class_exists('SassParser'))
+{
+	$sass_dir = $config['lib_dir']. 'sass';
+	
+	if (!$sass_dir)
+	{
+		throw new Exception("Cannot find phamlp's SASS directory.");
+	}
+
+	require_once(Kohana::find_file('vendor', $sass_dir.'/SassParser.php'));
+}
+
+
